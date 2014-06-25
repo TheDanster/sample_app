9288bd2560541be258861b43922d841c99bf5dd5
TheDanster <danny@unomee.com>
2014-06-25 15:58:08 +0100

    help me obiwan you're my only hope

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20140625132935) do
-=======
-ActiveRecord::Schema.define(version: 20140624144529) do
->>>>>>> sign-up
 
   create_table "users", force: true do |t|
     t.string   "name"
